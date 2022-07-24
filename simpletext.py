e92ab3125eba657b3daf16f8492fffcc39b568dd
jonasbrocas <55618670+jonasbrocas007@users.noreply.github.com>
2022-07-24 13:02:19 +0100

    Delete simpletext.py

--- simpletext.py
+++
@@ -1,9 +0,0 @@
-#simple text compiler from the .bin file
-f = open('code.bin', 'r')
-loser = f.readlines()
-new_loser = ''
-
-#make the null chars disapper and prints the filtered bin file
-for char in loser: 
-    new_loser += char
-print(new_loser)
